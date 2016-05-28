108b821ea5fd3c73d09e6093c88917f6fb2fbe24
Mantas <Mantas@Manto>
2016-05-28 15:21:48 +0300

    process ir descriptor

--- MOS/src/os/ProcessDescriptor.java
+++ MOS/src/os/ProcessDescriptor.java
@@ -5,10 +5,62 @@
  */
 package os;
 
+import java.util.LinkedList;
+import os.OS.ProcName;
+import os.OS.ProcessState;
+import os.OS.ResName;
+import os.machine.CPU;
+
 /**
  *
  * @author Mantas
  */
 public class ProcessDescriptor {
-    
+
+	public LinkedList<Process> processList;
+	public LinkedList<Process> childrenList;
+	
+	public LinkedList<Resource> createdResList;
+	public LinkedList<Resource> ownedResList;
+	public LinkedList<ResName> waitingFor;
+	
+	public int intId;
+	public ProcName extId;
+	public String pName;
+	public Process myProc;
+	public Process parentProcess;
+	public int priority;
+	
+	public CPU cpu;
+	public OS os;
+	
+	public ProcessState pState;
+	public RegState savedState;
+	
+	public ProcessDescriptor(int intId, ProcName extId, String pName,
+			LinkedList<Process> processList,
+			Process parentProcess,
+			Process myProcess,
+			CPU cpu, OS os,
+			ProcessState pState, int priority) {
+		this.intId = intId;
+		this.extId = extId;
+		this.pName = pName;
+		this.priority = priority;
+		this.myProc = myProcess;
+		
+		this.processList = processList;
+		this.parentProcess = parentProcess;
+		this.cpu = cpu;
+		this.os = os;
+		
+		this.savedState = new RegState();
+		this.pState = ProcessState.READY;
+		
+		childrenList = new LinkedList<>();
+		createdResList = new LinkedList<>();
+		ownedResList = new LinkedList<>();
+		waitingFor = new LinkedList<>();
+	}
 }
+
