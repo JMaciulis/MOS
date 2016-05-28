6f3884c8244843536b6669ded2d2b4fba4f991e5
Justinas <just.maciulis@gmail.com>
2016-05-28 17:20:03 +0300

    RegState puse / Memory

--- MOS/src/os/RegState.java
+++ MOS/src/os/RegState.java
@@ -5,10 +5,31 @@
  */
 package os;
 
+import os.virtualmachine.registers.Register2B;
+import os.virtualmachine.registers.Register4B;
+import os.virtualmachine.registers.RegisterInterrupt;
+import os.virtualmachine.registers.RegisterLogic;
+
 /**
  *
  * @author Mantas
  */
 public class RegState {
+	
+    public Register2B regKS;
+    public Register2B regSV;
+    public Register4B regSK;
+
+    public RegisterInterrupt regSI;
+    public RegisterInterrupt regPI;
+    
+    public RegState() {
+    	this.regKS = new Register2B();
+    	this.regSV = new Register2B();
+    	this.regSK = new Register4B();
+    	
+    	this.regSI = new RegisterInterrupt();
+    	this.regPI = new RegisterInterrupt();
+    }
     
 }
